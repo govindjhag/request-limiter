9ee2be61f131cd4f9e89fbeef36ec3c237cc97c3
Govind <govind.jha@imerit.net>
2019-01-14 18:18:37 +0530

    req-limiter added

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 /**
  * Created by govind on 9/1/19.
  */
-'use strict';
+"use strict";
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache( { stdTTL: 10, checkperiod: 10 } );
+module.exports.init  = (request,reply) =>{
 
-module.exports.isUserRequested = function(no){
-    return no+no;
+    console.log(request);
+
+    let value = myCache.get( request.auth.credentials.contID+"_"+request.path );
+
+
+    if ( value == undefined ){
+
+        let obj = { "reqCount": 1 };
+
+        let cacheName = request.auth.credentials.contID+"_"+request.path;
+        myCache.set( cacheName, obj, function( err, success ){
+            if( !err && success ){
+                reply(success)
+                // true
+                // ... do something ...
+            }
+        });
+    }else{
+        if(value.reqCount >1){
+            reply({message:'Request limit excedded',appcode:50005}).code(429).takeover();
+        }else{
+            value.reqCount+=1;
+            let cacheName = request.auth.credentials.contID+"_"+request.path;
+            myCache.set( cacheName, value, function( err, success ){
+                if( !err && success ){
+                    reply(success)
+                }
+            });
+        }
+
+    }
+
 };
---
+++ readme.md
@@ -0,0 +1,1 @@
+#This is an example I am trying to publish node npm modulw
